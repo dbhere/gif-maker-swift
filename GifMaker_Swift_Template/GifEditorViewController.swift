429e1b5e0698269a5d4edd876cde3b6624dc4c07
alexscott <alexzhangshuo@gmail.com>
2019-03-03 23:34:19 +0800

    add captionTextField delegate

--- GifMaker_Swift_Template/GifEditorViewController.swift
+++ GifMaker_Swift_Template/GifEditorViewController.swift
@@ -14,9 +14,27 @@ class GifEditorViewController: UIViewController {
     @IBOutlet weak var captionTextField: UITextField!
     var gif: Gif?
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        captionTextField.delegate = self
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         gifImageView.image = gif?.gifImage
+    }
+}
+
+// MARK: - TextFieldDelegate methods
+extension GifEditorViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.placeholder = nil
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
