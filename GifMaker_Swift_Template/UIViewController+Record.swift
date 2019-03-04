0d2099ee6631396cb4faf672470b1ac7b3a6f62b
alexscott <alexzhangshuo@gmail.com>
2019-03-04 18:46:00 +0800

    录制按钮提供选择拍摄/相册/取消

--- GifMaker_Swift_Template/UIViewController+Record.swift
+++ GifMaker_Swift_Template/UIViewController+Record.swift
@@ -15,11 +15,49 @@ let delayTime  = Float(0.2)
 let loopCount  = 0    // 0 means loop forever
 
 extension UIViewController {
-    @IBAction func launchVideoCamera(sender: AnyObject) {
+    // MARK: - 录制按钮
+    //录制按钮提供三种选择：相册，拍摄，取消
+    @IBAction func presentVideoOptions(_ sender: AnyObject) {
+        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+            launchPhotoLibrary()
+            return
+        }
+        
+        //呈现alertVC
+        let newGifActionSheet = UIAlertController(title: "创建新gif", message: nil, preferredStyle: .actionSheet)
+        let recordVideoAction = UIAlertAction(title: "录像", style: .default) { (action) in
+            self.launchVideoCamera()
+        }
+        let chooseFromLibraryAction = UIAlertAction(title: "相册", style: .default) { (action) in
+            self.launchPhotoLibrary()
+        }
+        let cancelAction = UIAlertAction(title: "取消", style: .cancel, handler: nil)
+        
+        newGifActionSheet.addAction(recordVideoAction)
+        newGifActionSheet.addAction(chooseFromLibraryAction)
+        newGifActionSheet.addAction(cancelAction)
+        
+        present(newGifActionSheet, animated: true, completion: nil)
+        newGifActionSheet.view.tintColor = UIColor(red: 255.0/255.0, green: 65.0/255.0, blue: 112.0/255.0, alpha: 1.0)
+    }
+    
+    func launchVideoCamera() {
         let recordVideoController = UIImagePickerController()
         
         //set sourceType, mediaTypes, allowsEditing, delegate
         recordVideoController.sourceType = .camera
+        recordVideoController.mediaTypes = [kUTTypeMovie as String]
+        recordVideoController.allowsEditing = false
+        recordVideoController.delegate = self
+        
+        present(recordVideoController, animated: true, completion: nil)
+    }
+    
+    func launchPhotoLibrary() {
+        let recordVideoController = UIImagePickerController()
+        
+        //set sourceType, mediaTypes, allowsEditing, delegate
+        recordVideoController.sourceType = .photoLibrary
         recordVideoController.mediaTypes = [kUTTypeMovie as String]
         recordVideoController.allowsEditing = false
         recordVideoController.delegate = self
